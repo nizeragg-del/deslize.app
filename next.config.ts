8afe4e5e895b793004d3505f2eba8b11d3d3aff1
nizeragg-del <nizeragg@gmail.com>
2026-05-22 13:46:48 -0300

    Set Next tracing root for Vercel builds

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  distDir: ".next",
+  outputFileTracingRoot: process.cwd(),
 };
 
 export default nextConfig;
